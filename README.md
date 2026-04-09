b1f117fb75a679a5706b85fc0540005d7b29d1ed
VictorViapiana <victorviapianafonseca1@gmail.com>
2026-04-08 21:38:04 -0300

    Primeiro commi - organizador de arquivos

---
+++ README.md
@@ -0,0 +1,26 @@
+# 📂 Organizador de Arquivos em Python
+
+Este projeto organiza automaticamente arquivos de uma pasta com base em suas extensões.
+
+## 🚀 Funcionalidades
+
+- Move arquivos automaticamente
+- Separa por categorias:
+  - PDF
+  - Imagens
+  - Vídeos
+  - Outros
+
+## 🛠 Tecnologias
+
+- Python
+- Bibliotecas: os, shutil
+
+## ▶️ Como usar
+
+1. Clone o repositório
+2. Altere o caminho da pasta no código
+3. Execute:
+
+```bash
+python main.py
---
+++ main.py
@@ -0,0 +1,41 @@
+import os
+import shutil
+
+caminho = r"C:\Users\victo\Downloads"
+
+def organizar_arquivos(caminho):
+    arquivos = os.listdir(caminho)
+
+    # Criar pastas
+    pastas = {
+        "PDF": ["pdf"],
+        "Imagens": ["jpg", "jpeg", "png"],
+        "Videos": ["mp4", "mp3", "mov", "avi"],
+        "Outros": []
+    }
+
+    for pasta in pastas:
+        os.makedirs(os.path.join(caminho, pasta), exist_ok=True)
+
+    for arquivo in arquivos:
+        caminho_arquivo = os.path.join(caminho, arquivo)
+
+        if os.path.isfile(caminho_arquivo) and "." in arquivo:
+            extensao = arquivo.split(".")[-1].lower()
+
+            movido = False
+
+            for pasta, extensoes in pastas.items():
+                if extensao in extensoes:
+                    destino = os.path.join(caminho, pasta)
+                    shutil.move(caminho_arquivo, os.path.join(destino, arquivo))
+                    movido = True
+                    break
+
+            if not movido:
+                destino = os.path.join(caminho, "Outros")
+                shutil.move(caminho_arquivo, os.path.join(destino, arquivo))
+
+    print("Arquivos organizados com sucesso!")
+
+organizar_arquivos(caminho)
